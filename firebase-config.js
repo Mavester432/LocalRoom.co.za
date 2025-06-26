73cfdf556d382f2c5d4580adeb810e31542b9212
Mavester432 <122857845+Mavester432@users.noreply.github.com>
2025-06-26 08:57:13 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,5 @@
+// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
@@ -7,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCqTJ7hNrVqO_mJ4cfqb5fOj-pvqzq7qII",
   authDomain: "localroomapp.firebaseapp.com",
   projectId: "localroomapp",
-  storageBucket: "localroomapp.appspot.com",  // Fixed this line to use the correct domain for storage
+  storageBucket: "localroomapp.appspot.com",
   messagingSenderId: "232945388743",
   appId: "1:232945388743:web:089083c9f60269406c02e2",
   measurementId: "G-XBC1GV59MF"
